57eadbca2f73d3917935dc10c7770de3210b495e
TeoGia <teogia89@gmail.com>
2015-04-07 19:56:52 +0300

    CRMClient-2 Client Authentication.
    to be continued..:P

---
+++ src/main/java/ged/daedaluswin/crmclient/UI/LoginScreen.java
@@ -0,0 +1,13 @@
+package ged.daedaluswin.crmclient.UI;
+
+import javax.swing.*;
+
+/**
+ * Created by TeoGia on 30 March 2015.
+ */
+public class LoginScreen {
+    private JPanel loginPanel;
+    private JTextField userNameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+}
